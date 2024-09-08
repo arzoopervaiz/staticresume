742089f2cbbf3e428cda2015b28c63fb8c25ad5c
John Doe <john@example.com>
2024-09-08 15:53:29 +0500

    Your commit message

---
+++ app.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+document.getElementById('toggle-skills').addEventListener('click', function () {
+    var skillsSection = document.getElementById('skills');
+    if (skillsSection.style.display === 'none') {
+        skillsSection.style.display = 'block';
+    }
+    else {
+        skillsSection.style.display = 'none';
+    }
+});
---
+++ app.ts
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+document.getElementById('toggle-skills')!.addEventListener('click', () => {
+    const skillsSection = document.getElementById('skills');
+    if (skillsSection!.style.display === 'none') {
+      skillsSection!.style.display = 'block';
+    } else {
+      skillsSection!.style.display = 'none';
+    }
+  });
